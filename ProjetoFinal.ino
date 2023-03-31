f5a11091af8221c1765c0795ca021bd8b3660ac5
Ana Luiza Fioravante Di Sciascio <analuiza197@gmail.com>
2023-03-30 22:29:38 -0300

    add: pisca e etc

--- ProjetoFinal.ino
+++ ProjetoFinal.ino
@@ -1,12 +1,15 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-  int potencia = 1; // valor inicial da potência
-  int minutos = 0; // valor inicial dos minutos
-  int segundos = 0; // valor inicial dos segundos
-  int segundos_totais = 0; // valor total de segundos a serem contados pelo timer
-  boolean timer_running = false; // flag para saber se o timer está rodando
-  int screen = 0;
+int potencia = 1; // valor inicial da potência
+int minutos = 0; // valor inicial dos minutos
+int segundos = 0; // valor inicial dos segundos
+int segundos_totais = 0; // valor total de segundos a serem contados pelo timer
+boolean timer_running = false; // flag para saber se o timer está rodando
+int screen = 0;
+int cursor = 0; 
+int tempoPisca = 0;
+bool ledLigado = true;
 
 void reset() {
   potencia = 1; // valor inicial da potência
@@ -23,53 +26,73 @@ void setup() {
   lcd.begin(16, 2);
   lcd.print("Microondas");
   lcd.setCursor(0, 1);
-  lcd.print("Clique RIGHT");
+  lcd.print("Clique SELECT");
 }
 
 void loop() {
+  Serial.println(analogRead(0));
   // esperar pelo clique do botão direito para passar para a próxima tela
   if(screen == 0) {
-    if (analogRead(0) < 80) {
+    if (analogRead(0) > 700 && analogRead(0) < 800) { //Select
       screen = 1;
-      delay(500);
+      delay(300);
     }
   } else if (screen == 1) {
     // tela de seleção de potência
     lcd.clear();
     lcd.print("Potencia");
     lcd.setCursor(0, 1);
-    if (potencia < 10) lcd.print("0");
-    lcd.print(potencia);
+    if (tempoPisca == 5) {
+      ledLigado = !ledLigado;
+      tempoPisca = 0;
+    }
+    if(!ledLigado) {
+      if (cursor == 0) {
+        lcd.print(" ");
+        lcd.print(potencia < 10 ? potencia : potencia%10);
+      } else {
+        lcd.print(potencia < 10 ? 0 : potencia/10);
+        lcd.print(" ");
+      }
+    } else {
+      if (potencia < 10) lcd.print("0");
+      lcd.print(potencia);
+    }
     if (analogRead(0) < 80) {
-      screen = 2;
-      delay(500);
+      right();
     } else if (analogRead(0) < 200) {
       upPotencia();
     } else if (analogRead(0) < 400) {
       downPotencia();
     } else if (analogRead(0) < 600) {
       left();
-      delay(300);
+    } else if (analogRead(0) < 800) {
+      select();
     }
-    delay(100);
+    delay(100); //Se não colocar delay, a tela fica piscando
+    tempoPisca++;
   } else if (screen == 2) {
     // tela de seleção de tempo
     lcd.clear();
     lcd.print("Tempo");
-    print_time();
+    if (tempoPisca == 5) {
+      ledLigado = !ledLigado;
+      tempoPisca = 0;
+    }
+    print_time(false);
     if (analogRead(0) < 80) {
-      screen = 3;
-      delay(500);
+      right();
     } else if (analogRead(0) < 200) {
       upTempo();
-      delay(300);
     } else if (analogRead(0) < 400) {
       downTempo();
     } else if (analogRead(0) < 600) {
       left();
-      delay(300);
+    } else if (analogRead(0) < 800) {
+      select();
     }
-    delay(100);
+    delay(100); //Se não colocar delay, a tela fica piscando
+    tempoPisca++;
   } else if (screen == 3) {
     // iniciar contagem regressiva
     lcd.clear();
@@ -77,19 +100,23 @@ void loop() {
     segundos_totais = minutos * 60 + segundos;
     timer_running = true;
     while (timer_running) {
-      print_time();
+      print_time(true);
       delay(1000);
       segundos_totais--;
       minutos = segundos_totais/60;
       segundos = segundos_totais%60;
-      if(segundos == 9) {
-        lcd.clear();
-        lcd.print("Contando");
-      }
       if (segundos_totais < 0) {
+        timer_running = false;
+        screen++;
+      }
+      //TODO: Permitir que a pessoa volte a tela
+      Serial.println(analogRead(0));
+      if (analogRead(0) > 400 && analogRead(0) < 600) { //Left
+        left();
         timer_running = false;
       }
     }
+  } else if (screen == 4) {
     lcd.clear();
     lcd.print("Fim");
     reset();
@@ -102,38 +129,95 @@ void loop() {
 
 
 void upPotencia() {
-  if (potencia < 99) potencia++;
+  if (potencia < 90 && cursor == 0) {
+    potencia = potencia+10;
+  } else if (potencia < 99) {
+    if (cursor == 0) cursor = 1;
+    potencia++;
+  }
+  delay(100);
 }
 
 void upTempo() {
-  if (minutos < 60) {
+  if (minutos < 59 && cursor == 0) {
     minutos++;
-    print_time();
-  } 
+  } else if (cursor == 1) {
+    if (segundos == 59) {
+      if (minutos < 59) {
+        minutos++;
+        segundos = 0;
+      }
+    } else {
+      segundos++;
+    }
+  }
+  delay(100);
 }
 
 void downPotencia() {
   if (potencia > 1) {
     potencia--;
-    lcd.setCursor(0, 1);
-    lcd.print(potencia);
+    delay(100);
   }
 }
 
 void downTempo() {
-  if (minutos > 0) minutos--;
+  if (minutos > 0 && cursor == 0) {
+    minutos--;
+  } else if (cursor == 1) {
+    if (segundos > 0) {
+      segundos--;
+    } else if (minutos > 0) {
+      minutos--;
+      segundos = 59;
+    }
+  }
+  delay(100);
 }
 
 void left() {
-  if(screen > 0) screen--;
-  if (screen == 0) setup();
+  if(screen > 0 && cursor == 0) {
+    Serial.println("Passou");
+    screen--;
+    delay(200);
+    if (screen == 0) setup();
+  } else {
+    cursor = 0;
+    delay(200);
+  }
 }
 
-void print_time() {
+void right() {
+  cursor = cursor == 0 ? 1 : 0;
+  delay(100);
+}
+
+void select () {
+  screen++;
+  cursor = 0;
+  delay(100);
+}
+
+void print_time(bool ignore) {
   lcd.setCursor(0, 1);
-   if(minutos < 10) lcd.print("0");
-  lcd.print(minutos);
-  lcd.print(":");
-  if(segundos < 10) lcd.print("0");
-  lcd.print(segundos);
+
+  if(!ledLigado && !ignore) {
+    if (cursor == 0) {
+      lcd.print("  :");
+      if(segundos < 10) lcd.print("0");
+      lcd.print(segundos);
+    } else {
+      if(minutos < 10) lcd.print("0");
+      lcd.print(minutos);
+      lcd.print(":  ");
+    }
+  } else {
+    if(minutos < 10) lcd.print("0");
+    lcd.print(minutos);
+    lcd.print(":");
+    if(segundos < 10) lcd.print("0");
+    lcd.print(segundos);
+  }
+
+
 }
